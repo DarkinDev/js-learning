1d37f236d04fa1b8929d9fc5caf6457df4ee5a87
Steven <darkin6336@gmail.com>
2025-05-03 15:02:03 +0700

    Add: comment code + update clock funtion

--- Forecastly/script.js
+++ Forecastly/script.js
@@ -1,13 +1,16 @@
 const city_name = "ha noi";
 const API_key = "806c10017721c07fa7a1f6804f00d0b9";
+// api call for current location weather
 const url = `http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}&lang=vi&units=metric`;
+
+// api call for current forecast per 3 hrs/ 5 days
 const forecasturl = `https://api.openweathermap.org/data/2.5/forecast?q=${city_name}&appid=${API_key}`
 const tempElement = document.getElementsByClassName("temperature")[0];
 const degreeElement = document.getElementsByClassName("degree")[0];
 const dateElement = document.getElementsByClassName("date")[0];
 const weekdayElement = document.getElementsByClassName("weekday-n-time")[0];
 
-let iniData;
+// get user current location
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(success, error);
@@ -16,14 +19,18 @@ function getLocation() {
   }
 }
 
+// get user location (longitude & latitude)
 function success(position) {
   const lon = position.coords.longitude;
   const lat = position.coords.latitude;
-  let temp;
-  console.log(temp);
-
+  const cnt = 7
+  // api call for current location weather
   const CoordUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&lang=vi&units=metric`;
+  
+  // api call for current forecast per 3 hrs/ 5 days
   const ForecastCoordUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_key}&lang=vi&units=metric`
+  
+  // get data for user location weather
   fetch(CoordUrl)
     .then((res) => res.json())
     .then((data) => {
@@ -36,12 +43,11 @@ function success(position) {
       console.error(error.message)
     });
 
-    console.log(temp);
-  // api call for forecast in 3h
+  // api call for forecast
   fetch(ForecastCoordUrl)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data)
+      console.log(data);
     })
 }
 
@@ -79,17 +85,20 @@ switch(d) {
     break;
 
 }
-let hours = today.getHours()
 
-let min = today.getMinutes()
-// them so 0 khi so phut be hon 10
-if(min < 10) {
-  min = '0' + min;
+function updateClock() {
+  let today = new Date()
+  let hours = today.getHours()
+  let min = today.getMinutes()
+  // them so 0 khi so phut be hon 10
+  if(min < 10) {
+    min = '0' + min;
+  }
+  setTimeout(updateClock, 1000)
+  weekdayElement.innerHTML = `${DayofWeek} | ${hours}:${min}`
 }
 
-weekdayElement.innerHTML = `${DayofWeek} | ${hours}:${min}`
-
-
+updateClock();
 
 function error() {
   alert("Sorry, no position available.");
